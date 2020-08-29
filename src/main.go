9d5fda492297370571fa7eaf19dfe807d75908c0
Starz0r <starz0r@starz0r.com>
2020-08-29 00:02:26 -0500

    main: make a new client and establish a connection

---
+++ src/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"github.com/spidernest-go/logger"
+	"os"
+)
+
+func main() {
+	logger.Info().Msg("AutoMod 0.1.0 Starting Up.")
+
+	// open a new discord connection
+	discord, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Msg("Initial Discord connection was refused.")
+	}
+
+	err = discord.Open()
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Msg("Discord websocket connection could not be established.")
+	}
+
+	// stay connected until interrupted
+	<-make(chan struct{})
+
+}
