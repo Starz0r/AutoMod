9f1586652c344dcea1db4496fe31e7f1f00f6f35
Starz0r <starz0r@starz0r.com>
2021-04-06 00:38:12 -0500

    membership: revert 880fd72

--- src/membership.go
+++ src/membership.go
@@ -12,17 +12,17 @@ func evtJoin(s *discordgo.Session, j *discordgo.GuildMemberAdd) {
 	logger.Debug().Msg("New User Join Event.")
 	// build user info string
 	userinfo := strings.Join([]string{"Username: ",
-		j.Member.User.Username, "#",
-		j.Member.User.Discriminator,
-		"\nID: ", j.Member.User.ID,
+		j.User.Username, "#",
+		j.User.Discriminator,
+		"\nID: ", j.User.ID,
 		"\nTimestamp: ", time.Now().UTC().Format(time.RFC1123)}, "")
 
 	// get avatar and set a default one if it doesn't exist
 	avatar := "http://is1.mzstatic.com/image/thumb/Purple117/v4/a1/d8/3a/a1d83a42-e84e-5965-c006-610fb8a1fd45/source/300x300bb.jpg"
-	if j.Member.User.Avatar != "" {
+	if j.User.Avatar != "" {
 		avatar = strings.Join([]string{"https://cdn.discordapp.com/avatars",
-			j.Member.User.ID, "/",
-			j.Member.User.Avatar, ".webp?size=256"}, "")
+			j.User.ID, "/",
+			j.User.Avatar, ".webp?size=256"}, "")
 	}
 
 	msg := NewEmbed().
@@ -43,17 +43,17 @@ func evtPart(s *discordgo.Session, j *discordgo.GuildMemberRemove) {
 	logger.Debug().Msg("New User Part Event.")
 	// build user info string
 	userinfo := strings.Join([]string{"Username: ",
-		j.Member.User.Username, "#",
-		j.Member.User.Discriminator,
-		"\nID: ", j.Member.User.ID,
+		j.User.Username, "#",
+		j.User.Discriminator,
+		"\nID: ", j.User.ID,
 		"\nTimestamp: ", time.Now().UTC().Format(time.RFC1123)}, "")
 
 	// get avatar and set a default one if it doesn't exist
 	avatar := "http://is1.mzstatic.com/image/thumb/Purple117/v4/a1/d8/3a/a1d83a42-e84e-5965-c006-610fb8a1fd45/source/300x300bb.jpg"
-	if j.Member.User.Avatar != "" {
+	if j.User.Avatar != "" {
 		avatar = strings.Join([]string{"https://cdn.discordapp.com/avatars",
-			j.Member.User.ID, "/",
-			j.Member.User.Avatar, ".webp?size=256"}, "")
+			j.User.ID, "/",
+			j.User.Avatar, ".webp?size=256"}, "")
 	}
 
 	msg := NewEmbed().
