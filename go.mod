ab827df82e6cdfa1e859e9abd806cd3a3098e31e
Starz0r <starz0r@starz0r.com>
2021-04-06 01:45:27 -0500

    database: open a connection

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.11
 
 require (
 	github.com/bwmarrin/discordgo v0.22.0
+	github.com/lib/pq v1.10.0 // indirect
 	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
 	github.com/rs/zerolog v1.19.0 // indirect
+	github.com/spidernest-go/db v0.0.0-20190526235030-072cabf93805
 	github.com/spidernest-go/logger v0.0.0-20191128190838-520d89ea00af
 )
---
+++ src/database.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+
+	"github.com/spidernest-go/db/lib/sqlbuilder"
+	"github.com/spidernest-go/db/postgresql"
+	"github.com/spidernest-go/logger"
+)
+
+var DB sqlbuilder.Database
+
+func connectDatabase() {
+	conn := postgresql.ConnectionURL{
+		Database: os.Getenv("PQ_DB"),
+		Host:     os.Getenv("PQ_HOST"),
+		User:     os.Getenv("PQ_USER"),
+		Password: os.Getenv("PQ_PASS"),
+	}
+
+	err := *new(error)
+	DB, err = postgresql.Open(conn)
+	if err != nil {
+		logger.Panic().Msg("Opening the PostgreSQL connection failed.")
+	}
+}
